16f5bb3f37a3ff26549f67866820f1548029a878
danielmoret <dmoret17@gmail.com>
2022-09-20 15:24:59 +0000

    traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [color, setColor] = useState("");
+  const [activePurple, setActivePurple] = useState(false);
+
+  return (
+    <div className="traffic-light">
+      <div className="post"></div>
+      <div className="lights">
+        <div
+          className={color === "red" ? "light red brillo" : "light red"}
+          onClick={(event) => {
+            setColor("red");
+          }}
+        ></div>
+        <div
+          className={
+            color === "yellow" ? "light yellow brillo" : "light yellow"
+          }
+          onClick={(event) => {
+            setColor("yellow");
+          }}
+        ></div>
+        <div
+          className={color === "green" ? "light green brillo" : "light green"}
+          onClick={(event) => {
+            setColor("green");
+          }}
+        ></div>
+        <div
+          className={
+            activePurple
+              ? color === "purple"
+                ? "light purple brillo"
+                : "light purple"
+              : ""
+          }
+          onClick={(event) => {
+            setColor("purple");
+          }}
+        ></div>
+      </div>
+
+      <div className="mt-2">
+        <button
+          className="btn btn-primary me-2"
+          onClick={(event) => {
+            if (color === "red") {
+              setColor("yellow");
+            } else if (color === "yellow") {
+              setColor("green");
+            } else if (color === "green" && activePurple === true) {
+              setColor("purple");
+            } else {
+              setColor("red");
+            }
+          }}
+        >
+          Switch Color 🔁
+        </button>
+        <button
+          className={activePurple ? "btn btn-danger" : "btn btn-success"}
+          onClick={(event) => {
+            if (activePurple === false) setActivePurple(true);
+            else setActivePurple(false);
+          }}
+        >
+          {activePurple ? "Remove Purple Color" : "Add Purple Color"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
